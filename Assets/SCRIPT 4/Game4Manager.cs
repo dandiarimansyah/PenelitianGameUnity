f7165678824be5baf2b1842ecf9da397381efc52
Dandi Arimansyah <dandimansyah@gmail.com>
2022-03-26 02:35:12 +0700

    Done Susun Game 4

--- Assets/SCRIPT 4/Game4Manager.cs
+++ Assets/SCRIPT 4/Game4Manager.cs
@@ -7,22 +7,22 @@ using UnityEngine.UI;
 
 public class Game4Manager : MonoBehaviour
 {
-    public GameObject KategoriSoal;
-    public GameObject TulisanSoal;
-    public GameObject[] m_Gambar;
-    public GameObject[] m_Border;
-    public GameObject[] m_Button;
+    [SerializeField] private GameObject KategoriSoal;
+    [SerializeField] private GameObject TulisanSoal;
+    [SerializeField] private GameObject[] m_Gambar;
+    [SerializeField] private GameObject[] m_Border;
+    [SerializeField] private GameObject[] m_Button;
 
-    public Sprite[] m_KategoriSoal;
-    public Sprite[] m_TulisanSoal;
-    public Sprite[] m_SemuaHewan;
-    public Sprite[] m_Hewan_Darat;
-    public Sprite[] m_Hewan_Laut;
-    public Sprite[] m_Hewan_Tanduk;
-    public Sprite[] m_Hewan_Kaki2;
-    public Sprite[] m_Hewan_Kaki4;
-    public Sprite BorderBenar;
-    public Sprite BorderSalah;
+    [SerializeField] private Sprite[] m_KategoriSoal;
+    [SerializeField] private Sprite[] m_TulisanSoal;
+    [SerializeField] private Sprite[] m_SemuaHewan;
+    [SerializeField] private Sprite[] m_Hewan_Darat;
+    [SerializeField] private Sprite[] m_Hewan_Laut;
+    [SerializeField] private Sprite[] m_Hewan_Tanduk;
+    [SerializeField] private Sprite[] m_Hewan_Kaki2;
+    [SerializeField] private Sprite[] m_Hewan_Kaki4;
+    [SerializeField] private Sprite BorderBenar;
+    [SerializeField] private Sprite BorderSalah;
 
     [SerializeField] private AudioSource _source;
     [SerializeField] private AudioClip _benar, _salah, _gameover;
@@ -30,8 +30,11 @@ public class Game4Manager : MonoBehaviour
     HashSet<int> listMuncul = new HashSet<int>();
     HashSet<int> HewanTerpilih = new HashSet<int>();
     HashSet<int> Ditempati = new HashSet<int>();
-    private Sprite[] ArrayTerpilih;
+    HashSet<int> TempatBenar = new HashSet<int>();
+    HashSet<string> NamaHewanTerpilih = new HashSet<string>();
 
+    private Sprite[] ArrayTerpilih;
+    private Sprite[] KategoriTerpilih;
 
     public int jumlahSoal = 5;
     private int countSoal;
@@ -106,11 +109,16 @@ public class Game4Manager : MonoBehaviour
                 break;
         }
 
+        for (int i = 0; i < ArrayTerpilih.Count(); i++)
+        {
+            NamaHewanTerpilih.Add(ArrayTerpilih[i].name);
+        }
+
         //Jumlah hewan yang benar
         jumlahJawaban = Random.Range(3, 6);
         Debug.Log("jumlah = "+ jumlahJawaban);
 
-        SusunGambarJawaban(jumlahHewan, kategoriTerpilih, jumlahJawaban, ArrayTerpilih);
+        SusunGambarJawaban(jumlahJawaban, ArrayTerpilih);
     }
 
     public int RandomAll(int jumlah, HashSet<int> Array)
@@ -126,27 +134,47 @@ public class Game4Manager : MonoBehaviour
         return angka;
     }
 
-    public void SusunGambarJawaban(int jumlahHewan, int kategoriTerpilih, int jumlahJawaban, Sprite[] ArrayTerpilih)
+    public void SusunGambarJawaban(int jumlahJawaban, Sprite[] ArrayTerpilih)
     {
         //var angkaSebelum = -1;
         var randomTempat = 0;
         var randomHewan = 0;
+        string namaRandomHewan;
         var tempatTersisa = 10 - jumlahJawaban;
         var jmlHewanKategori = ArrayTerpilih.Count();
-        Debug.Log("JumlahArray = " + jmlHewanKategori);
+
+        Debug.Log("jumlahJawaban = " + jumlahJawaban);
 
         for (int i = 0; i < jumlahJawaban; i++)
         {
-            randomTempat = RandomAll(10, Ditempati);            
+            randomTempat = RandomAll(10, Ditempati);
+            TempatBenar.Add(Ditempati.ElementAt(i));
+
             randomHewan = RandomAll(jmlHewanKategori, HewanTerpilih);
 
-            Debug.Log("Tempat = "+randomTempat);
-            Debug.Log("Hewan = "+ randomHewan);
+            Debug.Log("HewanBenar --> "+ ArrayTerpilih[randomHewan].name);
 
             m_Gambar[randomTempat].GetComponent<Image>().sprite = ArrayTerpilih[randomHewan];
-
         }
-        
+
+
+        for (int i = 0; i < tempatTersisa; i++)
+        {
+            randomTempat = RandomAll(10, Ditempati);
+
+            do
+            {
+                randomHewan = Random.Range(0, jumlahHewan);
+                namaRandomHewan = m_SemuaHewan[randomHewan].name;
+            } while (NamaHewanTerpilih.Contains(namaRandomHewan));
+
+            Debug.Log("Sisa: " + namaRandomHewan);
+
+            NamaHewanTerpilih.Add(namaRandomHewan);
+
+            m_Gambar[randomTempat].GetComponent<Image>().sprite = m_SemuaHewan[randomHewan];
+        }
+
     }
 
 }
